aee30db24208634e94c1ffa78c874c415281e83d
khan036 <nayeem1505@gmail.com>
2019-05-26 12:15:22 +0600

    Splash Activity. Ok.

---
+++ app/src/main/java/com/example/achilis/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.achilis;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SystemClock.sleep(3000);
+        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+
+        startActivity(loginIntent);
+        finish();
+    }
+}
